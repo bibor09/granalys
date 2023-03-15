71377da9af64d0199175dd13fd35d4e8d0463891
adorjani.biborka <adorjani.biborka@gmail.com>
2023-03-15 14:09:31 +0200

    Implement lcom4 for one class

--- src/measures.py
+++ src/measures.py
@@ -1,7 +1,7 @@
 from gremlin_python.process.anonymous_traversal import GraphTraversalSource
 from gremlin_python.process.traversal import within
 from gremlin_python.process.graph_traversal import __
-from src.util import put_in_dict, find_clusters, find
+from src.util import put_in_dict, count_connected_components, find_non_related_methods
 
 # get comment and all lines of code ratio
 def comment_ratio(g: GraphTraversalSource):
@@ -24,13 +24,16 @@ def cyclomatic_complexity(g: GraphTraversalSource):
     print("Cyclomatic complexity:", complexity)
 
 
-# LCOM4 class cohesion
+# LCOM4 class cohesion: lack of cohesion in methods
 def lcom4(g: GraphTraversalSource):
-    # Count connected components in class
-    class_level = dict()
+    # For each method store used class-level variables and methods
+    # If the method doesn't access any class-level variable or method, it isn't included in the dictionary
+    pot_rel_methods = dict()
 
+    # Get every method from class
     methods = g.V().has("name", "FunctionDef").toList()
     method_names = g.V().has("name", "FunctionDef").values("value").toList()
+
     for i in range(len(methods)):
         # Looks for every descendant of method which has "self" in name
         descendants = g.V(methods[i]).emit().repeat(__.out()).has("id","self").to_list()
@@ -38,13 +41,20 @@ def lcom4(g: GraphTraversalSource):
         for d in descendants: 
             # Looks for every parent of those specific descendants
             parent = g.V(d).in_e().out_v().values("value").next()
-            put_in_dict(method_names[i], parent, class_level)
+            put_in_dict(method_names[i], parent, pot_rel_methods)
 
-    print(class_level)
-    find_clusters(class_level, method_names)
-    find(set(method_names), class_level)
+    # From the methods not included in the dictionary, remove the empty ones, which shouldn't count towards the final lcom4 value
+    non_related_methods = find_non_related_methods(method_names, pot_rel_methods)
+    empty_methods = 0
+    for method in non_related_methods:
+        v = g.V().has("value", method).next()
+        if is_empty(v, g):
+            empty_methods += 1
+
+    print("LCOM4 =", count_connected_components(pot_rel_methods, method_names) + len(non_related_methods) - empty_methods)
 
 def is_empty(m, g: GraphTraversalSource):
     descendants = g.V(m).emit().repeat(__.out()).values("name").to_list()
-    # print(descendants)
-    return descendants.sort() == ["Pass", "arguments"].sort()
+    descendants.remove("FunctionDef")
+    descendants.sort()
+    return descendants == ["Pass", "arguments"]
--- src/util.py
+++ src/util.py
@@ -1,27 +1,26 @@
 import networkx as nx
 
-def find_clusters(dic, all_methods):
+def count_connected_components(pot_rel_methods, all_methods):
     # Create a graph where each set(aka key) is represented by a vertex, and two vertices are connected by an edge if their corresponding sets have at least one common element.
     G = nx.Graph()
-    keys = list(dic.keys())
+    keys = list(pot_rel_methods.keys())
     G.add_nodes_from(keys)
+
     for i in range(len(keys)):
         for j in range(i + 1, len(keys)):
-            # If they have at least one common element
-            # Or if the element is a class-level method
-            if len(dic[keys[i]] & dic[keys[j]]) > 0 or keys[i] in all_methods:
+            # If they have at least one common element or the element is a class-level method
+            if len(pot_rel_methods[keys[i]] & pot_rel_methods[keys[j]]) > 0 or keys[i] in all_methods:
                 G.add_edge(keys[i], keys[j])
 
-    print(G.edges)
+    # for comp in nx.connected_components(G):
+    #     print("CONNECTED COMP", comp)
+    return nx.number_connected_components(G)
 
-    for comp in nx.connected_components(G):
-        print(comp)
-    print(nx.number_connected_components(G))
-
-# To find completely disconnected elements
-def find(elements, dic):
-    all = set.union(*list(dic.values()))
-    print(elements.difference(all))
+# To find completely disconnected elements, which are automatically non-related 
+def find_non_related_methods(all_methods, pot_rel_methods):
+    pot_rel_m = set(pot_rel_methods.keys())
+    all_m = set(all_methods)
+    return all_m.difference(pot_rel_m)
 
 def put_in_dict(key, value, dict):
     if key not in dict.keys():
